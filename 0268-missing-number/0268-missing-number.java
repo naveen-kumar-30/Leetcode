7d6ba6fa8868d6e117e45a1412779607dcf0a94d
naveen-kumar-30 <naveenkumarp3052004@gmail.com>
2024-11-23 21:00:01 +0530

    Time: 0 ms (100%), Space: 45.2 MB (45.67%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+     int n= nums.length;
+        int expect = (n * (n+1)/2);
+        int sum=0;
+        for(int i:nums){
+            sum += i;
+        }
+        return expect-sum;
+    }
+}
