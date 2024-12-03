e46bb2ae0df0009ff76c747b4aa23189c0318476
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2024-12-03 21:33:04 +0530

    Time: 0 ms (100%), Space: 43 MB (80.39%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-            int points = coordinates.length;
-               
-
-            int diffx = (coordinates[1][0] - coordinates[0][0]);
-            int diffy = (coordinates[1][1] - coordinates[0][1]);
-            int curr_diffx,curr_diffy;
-            for(int i=2;i<points;++i){
-                curr_diffx =  (coordinates[i][0] - coordinates[i-1][0]);
-                curr_diffy = (coordinates[i][1] - coordinates[i-1][1]);
-                if((diffx * curr_diffy )!= (diffy* curr_diffx))
+        int n = coordinates.length;
+        int diffx = coordinates[1][0] - coordinates[0][0];
+        int diffy = coordinates[1][1] - coordinates[0][1];
+        int curr_diffx,curr_diffy;
+        for(int i=2;i<n;++i){
+            curr_diffx = coordinates[i][0] - coordinates[i-1][0];
+            curr_diffy = coordinates[i][1] - coordinates[i-1][1];
+            if((diffy * curr_diffx) != (curr_diffy *diffx))
                     return false;
-            }
-            return true;
+            
+        }
+        return true;
     }
 }
