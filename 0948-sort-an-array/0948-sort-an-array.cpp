9b5a6891937157bed581291084ff55397cb25197
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2024-12-12 18:36:00 +0530

    Time: 2766 ms (5.01%), Space: 74.1 MB (56.63%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    
+    vector<int> sortArray(vector<int>& arr) {
+    qsort(arr,0,arr.size()-1);
+        return arr;
+    }
+    void qsort(vector<int> &arr,int low,int high){
+        if(low<high){
+           int Pindex = partion(arr,low,high);
+            qsort(arr,low,Pindex-1);
+            qsort(arr,Pindex+1,high);
+        }
+    }
     int partion(vector<int> &arr,int low,int high){
         int pivot = arr[low];
-        int i = low;
-        int j = high;
+        int i=low;
+        int j=high;
         while(i<j){
             while(arr[i]<=pivot && i<=high-1){
                 i++;
             }
             while(arr[j]>pivot && j>=low+1){
-                j--;        
+                j--;
             }
-            if(i<j) swap(arr[i],arr[j]);
+            if(i<j){
+                swap(arr[i],arr[j]); 
+            }
         }
-        swap(arr[low],arr[j]);
+            swap(arr[low],arr[j]);
         return j;
     }
 
-    void qs(vector<int> &arr,int low ,int high){
-        if(low<high){
-           int Pindex = partion(arr,low,high);
-           qs(arr,low,Pindex-1);
-           qs(arr,Pindex+1,high);
-        }
-    }
     
-    
-    vector<int> sortArray(vector<int>& arr) {
-        qs(arr,0,arr.size()-1);
-        return arr;
-    }
 };
