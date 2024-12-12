b407715c6094ce88b0ffcc3704765a81f006f486
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2024-12-12 19:35:30 +0530

    Time: 13 ms (7.8%), Space: 44.9 MB (12.39%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,10 +1,10 @@
 class Solution {
-    public String intToRoman(int num) {
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hundereds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String thousands[]={"","M","MM","MMM"};
-        
-        return thousands[num/1000] + hundereds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    public String intToRoman(int s) {
+        String [] one = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String [] ten = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String [] hundred = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String [] thoundsand = {"","M","MM","MMM"};
+
+        return thoundsand[s/1000] + hundred[(s%1000)/100] + ten[(s%100)/10] + one[s%10];
     }
-}
+    }
