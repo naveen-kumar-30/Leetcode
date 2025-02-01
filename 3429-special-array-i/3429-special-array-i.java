3c9da9814f80b210177568f4c36b68b59ba88841
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-02-01 09:28:32 +0530

    Time: 1 ms (37.58%), Space: 43.3 MB (72.64%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int count=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]%2==0 && nums[i-1]%2==1){
+                continue;
+            }
+            else if(nums[i]%2==1 && nums[i-1]%2==0){
+                continue;
+            }
+            else
+            return false;
+        }
+        return true;
+    }
+}
