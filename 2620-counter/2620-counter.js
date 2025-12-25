1fe2495c872bc8c079fc8ce66313c018e431c831
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-12-25 19:50:25 +0530

    Time: 39 ms (84.66%), Space: 54.6 MB (12.07%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -1,17 +1,7 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
+
 var createCounter = function(n) {
     // let count = n;
     return function() {
         return n++;
     };
 };
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
